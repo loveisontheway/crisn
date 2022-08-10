58a7fff86ddea445ac227358fe383ed6c7ff19e9
jiangjialiang <jiangjialiang0746@163.com>
2022-08-10 19:07:31 +0800

    Enum + Const

---
+++ crisn-common/src/main/java/com/crisn/common/constant/EpcConst.java
@@ -0,0 +1,16 @@
+package com.crisn.common.constant;
+
+/**
+ * EPC - Const
+ *
+ * @author jjl
+ * @date 2022/3/30
+ */
+public class EpcConst {
+
+    public static final String SEX = "SEX";
+    public static final String STAGE = "STAGE";
+    public static final String WELL = "WELL";
+    public static final String PROJECT_DEPT = "PROJECT_DEPT";
+
+}
