9006da7c6f43076efdc79fc446dadd45dcca8a11
jiangjialiang <jiangjialiang0746@163.com>
2022-08-15 09:07:52 +0800

    remove controller

--- crisn-epc/src/main/java/com/crisn/epc/controller/EpcProjectController.java
+++ crisn-epc/src/main/java/com/crisn/epc/controller/EpcProjectController.java
@@ -1,4 +1,4 @@
-package com.crisn.web.controller.epc;
+package com.crisn.epc.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
